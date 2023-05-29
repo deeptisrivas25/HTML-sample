ef114a0ebe59848a41cf0e42a193bfd6cfa5a637
deeptisrivas25 <133963119+deeptisrivas25@users.noreply.github.com>
2023-05-29 12:16:01 +0530

    2 files modified

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+
+//handle set interval with call back
 const btn1=document.querySelector("#btn1");
-
+let setTime;
 btn1.addEventListener("click",()=>{
-
-  setInterval(()=>{
+let stop=0;
+  setTime= setInterval(()=>{
+    console.log(stop);   
+    stop++;
     console.log("Hello")
   },1000)
+})
 
-  
+
+const btn2=document.querySelector("#btn2");
+btn2.addEventListener("click",()=>{
+clearInterval(setTime)
 })
